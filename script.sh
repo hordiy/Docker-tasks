991fe31d343457ba79aff6409b01a1e5bcfd59b3
Vladyslav Hordiienko <vladyslav_hordiienko@epam.com>
2020-09-03 18:32:37 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 git clone $1
-
 mvn -f $(find * -name pom.xml) clean package
-
-cp $(find / -type f -wholename "*target/*.jar") /home/app/demo.jar
-
-java -jar demo.jar
+cp $(find / -type f -wholename "*target/*.war") /home/app/demo.war
+java -jar demo.war
