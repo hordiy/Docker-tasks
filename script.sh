7560d5266fa8f382136fe08374ff6f72dd78bb88
Vladyslav Hordiienko <vladyslav_hordiienko@epam.com>
2020-09-04 09:02:46 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git clone $1
+package=war
+artifact=${2:-$package}
 mvn -f $(find * -name pom.xml) clean package
-cp $(find / -type f -wholename "*target/*.war") /home/app/demo.war
-java -jar demo.war
+cp $(find / -type f -wholename "*target/*.$artifact") /home/app/demo.$artifact
+java -jar demo.$artifact
